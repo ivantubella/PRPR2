72323f0787426e166a27a6d8ce339ed58d03a6a0
ivan.tubella <ivan.tubella@students.salle.url.edu>
2024-04-29 11:02:43 +0200

    Pokedex_GRAFICO actualizar, sin probar

---
+++ app/src/main/java/edu/url/salle/ivan/tubella/proyectofinal/Pokedex_GRAFICO.java
@@ -0,0 +1,16 @@
+package edu.url.salle.ivan.tubella.proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Pokedex_GRAFICO extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokedex_grafico);
+
+    }
+}
