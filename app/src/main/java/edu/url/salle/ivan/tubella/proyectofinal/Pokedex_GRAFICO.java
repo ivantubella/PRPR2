9cc95dc8695a5a2381e64fb04175e4b35c697955
Maxim Sanchez <maxim.sanchez@students.salle.url.edu>
2024-05-06 08:27:27 +0200

    Pokedex_GRAFICO actualizar, sin probar

--- app/src/main/java/edu/url/salle/ivan/tubella/proyectofinal/Pokedex_GRAFICO.java
+++ app/src/main/java/edu/url/salle/ivan/tubella/proyectofinal/Pokedex_GRAFICO.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.Image;
 import android.os.Bundle;
 import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+
+
 
 public class Pokedex_GRAFICO extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokedex_grafico);
+        ImageView imageView = null;
+        Picasso.get().load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png").into(imageView);
 
     }
 }
