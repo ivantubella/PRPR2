5ef6097242375491bd4d736c30e46bfa00a0793a
Alba Mayol Lozano <alba.mayol@students.salle.url.edu>
2024-05-06 13:58:08 +0200

    TabBar implementation DONE

--- app/src/main/java/edu/url/salle/ivan/tubella/proyectofinal/MainActivity.java
+++ app/src/main/java/edu/url/salle/ivan/tubella/proyectofinal/MainActivity.java
@@ -3,21 +3,39 @@ package edu.url.salle.ivan.tubella.proyectofinal;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.widget.Button;
+
+import edu.url.salle.ivan.tubella.proyectofinal.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new PokeBallFragment());
 
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.pokeball) {
+                replaceFragment(new PokeBallFragment());
+            } else if (item.getItemId() == R.id.trainer) {
+                replaceFragment(new TrainerFragment());
+            } else if (item.getItemId() == R.id.store) {
+                replaceFragment(new StoreFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.mainActivity);
-        if (fragment == null) {
-            fragment = new TabBarFragment();
-            fm.beginTransaction().add(R.id.mainActivity, fragment).commit();
-        }
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frame_layout, fragment);
+        ft.commit();
     }
 }
